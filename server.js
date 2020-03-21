588deef466ce05cbb01b81e172a25a0676a1a3dc
mamta <mamta@getvokal.com>
2020-03-21 20:37:42 +0530

    fixed img layout

--- server.js
+++ server.js
@@ -12,10 +12,9 @@ var app = express();
 app.set('view engine', 'ejs');
 const nodeHtmlToImage = require('node-html-to-image')
 
-app.get('/healthcheck', function(req, res)
-{
+app.get('/healthcheck', function (req, res) {
 
-res.send("html to img")
+    res.send("html to img")
 
 
 });
@@ -23,7 +22,7 @@ res.send("html to img")
 app.get('/convertHtml2image', function (req, res) {
     nodeHtmlToImage({
         output: './banner.png',
-        html: '<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><meta name="viewport" content="width=device-width, initial-scale=1.0"><meta http-equiv="X-UA-Compatible" content="ie=edge"><title>Banner</title><style>.container{background-color:rgb(255,252,209);width:100%;padding:2rem}.row{width:100%;display:flex;flex-direction:row;align-items:center;justify-content:center}[class^="col-"]{padding:0 1.5rem}.col-2{width:16.666}.col-10{width:83.33}.img{border-radius:50%;width:13rem;height:13em;object-fit:cover}p{margin:10px 0;font-size:2.7rem;color:red;font-weight:700;font-family:sans-serif}</style></head><body><div id="html-content" class="container"><div class="row"><div class="col-2"><img id="img" class="img" src={{image}} alt="image"/></div><div class="col-10"><p><span id="name">{{name}}</span> is a Top Answerer.</p><p>Recorded <span id="count">{{count}}</span> answers today.</p></div></div></div></body></html>',
+        html: '<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><meta name="viewport" content="width=device-width, initial-scale=1.0"><meta http-equiv="X-UA-Compatible" content="ie=edge"><title>Banner</title><style>.container{background-color:rgb(255,252,209);padding:2rem}.row{width:100%;display:flex;flex-direction:row;align-items:center;justify-content:center}[class^="col-"]{padding:0 1rem}.img{box-shadow: 3px 3px 8px grey;border-radius:50%;width:8rem;height:8em;object-fit:cover}p{margin:10px 0;font-size:2.2rem;color:red;font-weight:700;font-family:sans-serif}</style></head><body><div id="html-content" class="container"><div class="row"><div class="col-2"><img id="img" class="img" src={{image}} alt="image"/></div><div class="col-10"><p><span id="name">{{name}}</span> is a Top Answerer.</p><p>Recorded <span id="count">{{count}}</span> answers today.</p></div></div></div></body></html>',
         content: {
             name: req.query.name,
             count: req.query.count,
